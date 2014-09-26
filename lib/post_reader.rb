3ba7d3272cc278140aebd0e385b0510ca1336672
Katie <kab761@gmail.com>
2014-09-26 16:02:28 -0700

    navigation links in blog footer

--- lib/post_reader.rb
+++ lib/post_reader.rb
@@ -1,5 +1,6 @@
 class PostReader
-  attr_accessor :posts, :count
+  attr_accessor :count, :last_page
+  PER_PAGE = 5
 
   def initialize
     post_paths = Dir.glob("views/posts/*/*/*/*")
@@ -7,6 +8,7 @@ class PostReader
     @posts.sort_by! { |post| post.timestamp }
     @posts.reverse!
     @count = @posts.count
+    @last_page = (@count/PER_PAGE.to_f).ceil
   end
 
   def most_recent(n)
@@ -21,46 +23,90 @@ class PostReader
     @count == 0
   end
 
-  def first_page?(splat)
-    [nil, '', '1'].include? splat
+  def first_page?(page)
+    [0,1].include? page
   end
 
   def last_page?(page)
-    page * 5 >= @count
-  end
-
-  def last_page
-    (@count/5.0).ceil
+    page * PER_PAGE >= @count
   end
 
   def first_index_heading
     case @count
     when 0 then "I haven't made any posts!"
     when 1 then "My one lonely post:"
-    when 2..5 then "All #{@count} posts:"
-    else "Latest 5 posts:"
+    when 2..PER_PAGE then "All #{@count} posts:"
+    else "Latest #{PER_PAGE} posts:"
     end
   end
 
   def later_index_heading(page)
-    page = page.to_i
     if last_page?(page)
-      if page * 5 == @count
-        "Oldest 5 posts:"
+      if page * PER_PAGE == @count
+        "Oldest #{PER_PAGE} posts:"
       else
-        on_last_page = @count % 5
+        on_last_page = @count % PER_PAGE
         on_last_page == 1 ? "Oldest post:" : "Oldest #{on_last_page} posts:"
       end
     else
-      first = (page - 1) * 5 - 1
-      last = page * 5
-      "Posts \##{first} - \##{last}:"
+      first = (page - 1) * PER_PAGE + 1
+      last = page * PER_PAGE
+      "Posts \##{first} - #{last}:"
     end
+  end
+
+  def footer_links(page)
+    if @last_page <= 5
+      all_link_strings
+    elsif near_start?(page)
+      first_link_strings
+    elsif near_end?(page)
+      last_link_strings
+    else
+      mid_link_strings(page)
+    end
+  end
+
+  def near_start?(page)
+    page < 3
+  end
+
+  def near_end?(page)
+    page + 2 > @last_page
+  end
+
+  def all_link_strings
+    links = ""
+    (1..@last_page).each { |i| links += link_string(i) }
+    links
+  end
+
+  def first_link_strings
+    links = ""
+    (1..5).each { |i| links += link_string(i) }
+    links
+  end
+
+  def last_link_strings
+    links = ""
+    start = @last_page - 4
+    (start..@last_page).each { |i| links += link_string(i) }
+    links
+  end
+
+  def mid_link_strings(page)
+    links = ""
+    (page-2..page+2).each { |i| links += link_string(i) }
+    links
+  end
+
+  def link_string(i)
+    "<a href=\"/blog-index/#{i}\">#{i}</a> "
   end
 end
 
 class Post
-  attr_accessor :name, :path, :date, :timestamp, :contents
+  attr_accessor :name, :path, :date, :timestamp, :contents, :index_entry
 
   def initialize(post_path)
     @name = post_path.split('/').last
@@ -76,5 +122,7 @@ class Post
     @contents = file.read
     @timestamp = file.ctime
     @date = @timestamp.strftime "%Y/%m/%d"
+
+    @index_entry = "<li><a href=\"#{@path}\">#{@name}</a>   #{@date} </li><p>#{@contents.slice(0,50)}<p>"
   end
 end
